4b3b9969185a79fa8a3bf62a157835ebdf7470f2
Myck <136953823+Kmykh@users.noreply.github.com>
2026-09-09 13:09:59 -0500

    fix: configure JsonStringEnumConverter for enum serialization

--- src/AuraNova.API/Program.cs
+++ src/AuraNova.API/Program.cs
@@ -3,12 +3,17 @@ using AuraNova.Infrastructure.Persistence;
 using Microsoft.AspNetCore.RateLimiting;
 using AuraNova.API.Middlewares;
 using AuraNova.API.Configuration;
+using System.Text.Json.Serialization;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+builder.Services.AddControllers()
+    .AddJsonOptions(options =>
+    {
+        options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
+    });
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(options =>
